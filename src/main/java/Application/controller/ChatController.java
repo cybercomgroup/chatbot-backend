346af6e0c1c577932e31c51c24f9472a9f8dbb20
Joakim willard <joakimwillard@yahoo.se>
2017-11-09 16:53:17 +0100

    Created basic control method.

--- src/main/java/Application/controller/ChatController.java
+++ src/main/java/Application/controller/ChatController.java
@@ -1,5 +1,28 @@
 package Application.controller;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import Application.service.*;
+
+@RestController
+@RequestMapping(value = "/chatbot")
 public class ChatController {
+
+    private String request;
+
+    @RequestMapping(value = "/request")
+    public @ResponseBody
+    JSONObject Request(@RequestParam(value = "request") String request){
+        this.request = request;
+        WitaiService witaiService = new WitaiService();
+        witaiService.setPhrase(this.request);
+        JSONObject jobj = witaiService.getJsonResponse();
+        return jobj;
+    }
+
 
 }
